3f41215f5cb9437fc8feb2b776e5397d8f405f58
Fannytje <103598821+Fannytje@users.noreply.github.com>
2022-04-24 12:48:32 +0200

    Added search engine

--- src/app.js
+++ src/app.js
@@ -39,8 +39,19 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", (response.data.weather[0].description));
 }
 
-let apiKey = "3bbe90ca885623503d6c25ddf34132b7";
-let city = "Paris"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+    let apiKey = "3bbe90ca885623503d6c25ddf34132b7";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("Amsterdam");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
